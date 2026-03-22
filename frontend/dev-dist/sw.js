f8105d543c9abe33da7153510676f60af7b7cea0
Nithin Das <nithindaskavungal@gmail.com>
2026-03-22 13:47:49 +0530

    fix(backend):  code error

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.s0ut7d513kk"
+    "revision": "0.9paaca7gsdo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
